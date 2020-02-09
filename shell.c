c2384210e8de4ac5299edb7e0d0d0de800d2c4a1
Gavin <gavinwu23@gmail.com>
2020-02-09 00:57:03 -0600

    finished semicolon

--- shell.c
+++ shell.c
@@ -8,6 +8,16 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+//IMPORTANT TO KNOW I AM PARSING WITH SPACES IF THERE IS A SPECIAL token
+// EXAMPLE: ECHO HI > FOO.TXT
+// IT ONLY PICKS UP THE > SYMBOL IF IT IS SURROUNDED BY SPACES
+// same goes with the other symbols
+// if the autograder does it without spaces, i have to parse it differently
+// use a different delimminater
+
+// function decleration
+void semicolonCommand(char **arguments, int size, int index_pos);
+
 void convertToArray (char **arguments, char *input, int *size) {
   // need to prase my initialinput which is just a char array into individual words
   // place it into arguments []
@@ -93,6 +103,7 @@ char checkSymbol (char **arguments, int size, char **file_name, int *index){
     }
 
     else if (strcmp(arguments[i], ";") == 0){
+      *index = i;
       return ';';
     }
 
@@ -146,7 +157,7 @@ void runCommand(char **arguments, int size ) {
       execvp(newArg[0], newArg);
       break;
 
-    case '<' : ;
+    case '<' : ; // same concept as above but we only want to read it conents
       int ff = open (file_name, O_RDONLY);
       dup2(ff,0);
       char *newArg1[100];
@@ -159,7 +170,7 @@ void runCommand(char **arguments, int size ) {
       break;
 
     case ';' :
-      printf("; was called \n");
+      semicolonCommand(arguments, size, index_pos); // see below
       break;
 
     case '|' :
@@ -169,17 +180,67 @@ void runCommand(char **arguments, int size ) {
   } // switch case
 
 }
-//=======================================================================================
+
+//===========================================================================================================
+// arguments holds the array of input, into each individual words
+// size is the size of arguments
+// index_pos is the position on where the semi colon is at.
+// psuedo recursive call --> will call back runCommand to do work 
+void semicolonCommand(char **arguments, int size, int index_pos ) {
+
+  // create two different arrays for each command on each side
+  char *a[100];
+  char *b[100];
+
+  int a_size = 0;
+  int b_size = 0;
+
+  // grab the first half of the semicolon
+  int i;
+  for (i=0; i < index_pos; i++) {
+    a[i] = arguments[i];
+    a_size++;
+  }
+  a[index_pos] = NULL;
+  //runCommand(a,a_size);
+
+  // now grab the second half of the smicolon
+  int k;
+  for (k= index_pos + 1; k < size; k++){
+    b[b_size] = arguments[k];
+    b_size++;
+  }
+  b[b_size] = NULL;
+  //runCommand(b,b_size);
+
+  // now make another forking for the commands
+  pid_t pid;
+  pid = fork();
+  int childS;
+
+  // child process
+  if (pid == 0) {
+    runCommand(a,a_size); //  we want command A to go first
+  }
+
+  // parent i guess
+  else{
+      wait(&childS);
+      runCommand(b,b_size); // wait until the first command goes first, then run this 1
+  }
+
+}
+//======================================================================================================
 int main() {
 
   // make a singal for ctrl z too
   //signal (SIGINT, sigint_handler); // ctrl c
 
-  char userInput[100]; // intial userinput
+  char userInput[500]; // intial userinput
 
   // array of arguments, [0] is the command the the following are extra that comes along
   // for now set it to 100, change later base off initial input
-  char *arguments[100];
+  char *arguments[500];
 
   int numOfWords = 0; // useed in arguments where how many arguments we have in total
 
